29ce25472f73843d4a4a69b1d948e9d467db51c7
karlliedtke <kpliedtke24@coe.edu>
2025-03-13 19:53:35 +0000

    over under almsot done, wont break game after a win

--- over_under.py
+++ over_under.py
@@ -3,71 +3,62 @@ import random
 def rollDice():
     return random.randint(1,6) + random.randint(1,6) + random.randint(1,6)
 
-def over_under():
-    player1points = 0
-    player2points = 0
+def take_turn():
+        turn_total = 0
+        roll = rollDice()
+        print(roll)
+        while True:
+            guess = input("will next roll be over or under? over/under: ").strip().lower()
+            if guess in ["over", "under"]:
+                  break
+            else:
+                print("Invalid input, please enter 'over' or 'under'")
+        roll_two = rollDice()
+        print(roll_two)
+        if guess == "over" and roll_two > roll:
+            turn_total += 1
+            print()
+        elif guess == "under" and roll_two < roll:
+                turn_total += 1
+                
+        else:
+            turn_total +=0
 
+        return turn_total
+
+player1points = 0
+player2points = 0
+
+while True:
     while True:
-        print("\nplayer 1 is now rolling the dice")
-        roll = rollDice()
-        print(roll)
-        while True:
-            guess = input("Player 2, will next roll be over or under? over/under: ").strip().lower()
-            if guess in ["over", "under"]:
-                  break
-            else:
-                print("Invalid input, please enter 'over' or 'under'")
-        roll_two = rollDice()
-        print(roll_two)
-        if guess == "over" and roll_two > roll:
-            player2points += 1
-            print(f"\nplayer 1: {player1points} points")
-            print(f"player 2: {player2points} points")
-        elif guess == "under" and roll_two < roll:
-                player2points += 1
-                print(f"\nplayer 1: {player1points} points")
-                print(f"player 2: {player2points} points")
-        else:
-            print("\nNo points awarded")
-            print(f"player 1: {player1points} points")
-            print(f"player 2: {player2points} points")
-
-        if player2points ==6:
-                print("Game over, player 2 wins")
+        switch = input("It is now Player 1's turn to roll and player 2's to guess. enter yes to continue: ").strip().lower()
+        if switch in ["yes"]:
                 break
         else:
-                print("turn is over, switch player roles")
+            print("Invalid input, please enter 'yes'")
+    print(f"\nPlayer 1 is now rolling the dice")
 
-        print("\nplayer 2 is now rolling the dice")
-        roll = rollDice()
-        print(roll)
-        while True:
-            guess = input("Player 2, will next roll be over or under? over/under: ").strip().lower()
-            if guess in ["over", "under"]:
-                  break
-            else:
-                print("Invalid input, please enter 'over' or 'under'")
-        roll_two = rollDice()
-        print(roll_two)
-        if guess == "over" and roll_two > roll:
-            player1points += 1
-            print(f"\nplayer 1: {player1points} points")
-            print(f"player 2: {player2points} points")
-        elif guess == "under" and roll_two < roll:
-                player1points += 1
-                print(f"\nplayer 1: {player1points} points")
-                print(f"player 2: {player2points} points")
-        else:
-            print("\nNo points awarded")
-            print(f"player 1: {player1points} points")
-            print(f"player 2: {player2points} points")
-
-        if player1points ==6:
-                print("Game over, player 1 wins")
+    player2points += take_turn()
+    print(f"player2points: {player2points}")
+    if player2points ==2:
+            print("Game over, player 1 wins")
+            break
+    
+    while True:
+        switch = input("It is now Player 2's turn to roll and player 1's to guess. enter yes to continue: ").strip().lower()
+        if switch in ["yes"]:
                 break
         else:
-                print("turn is over, switch player roles")
+            print("Invalid input, please enter 'yes'")
+    print(f"\nPlayer 2 is now rolling the dice")
 
-over_under()
+    player1points += take_turn()
+    print(f"player1points: {player1points}")
+    if player1points ==2:
+            print("Game over, player 1 wins")
+            break
 
+take_turn()
+           
 
+      
